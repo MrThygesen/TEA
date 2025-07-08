13698554eec4893aec23d2a73a2b0183ad96fe1c
MrThygesen <mr.morten.thygesen@gmail.com>
2025-07-09 01:00:15 +0200

    Your commit message here

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,22 +1,13 @@
-const TerserPlugin = require('terser-webpack-plugin');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          exclude: /HeartbeatWorker\.js$/, // exclude this file from minification
-          terserOptions: {
-            compress: true,
-            mangle: true,
-            format: {
-              comments: false,
-            },
-          },
-          extractComments: false,
-        }),
-      ];
+    if (!isServer && config.optimization.minimizer) {
+      config.optimization.minimizer.forEach((plugin) => {
+        if (plugin.constructor.name === 'TerserPlugin') {
+          // Exclude HeartbeatWorker.js from minification
+          plugin.options.exclude = /HeartbeatWorker\.js$/;
+        }
+      });
     }
     return config;
   },
