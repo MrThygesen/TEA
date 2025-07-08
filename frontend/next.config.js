ae0882c60c14bc6a5980de5d3f7ad70001ebc617
MrThygesen <mr.morten.thygesen@gmail.com>
2025-07-09 00:50:26 +0200

    Fix: exclude HeartbeatWorker.js from Terser minification

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,22 @@
-// frontend/next.config.js
+const TerserPlugin = require('terser-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config, { isServer }) {
     if (!isServer) {
-      config.optimization.minimizer = config.optimization.minimizer.map((plugin) => {
-        if (plugin.constructor.name === 'TerserPlugin') {
-          plugin.options.exclude = /HeartbeatWorker\.js$/;
-        }
-        return plugin;
-      });
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          exclude: /HeartbeatWorker\.js$/, // exclude this file from minification
+          terserOptions: {
+            compress: true,
+            mangle: true,
+            format: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        }),
+      ];
     }
     return config;
   },
