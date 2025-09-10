a75413bc8cbaeac24e3895c1c39034d5cc840df0
MrThygesen <mr.morten.thygesen@gmail.com>
2025-09-10 10:44:54 +0200

    db updates

--- frontend/lib/email.js
+++ frontend/lib/email.js
@@ -12,7 +12,7 @@ export async function sendVerificationEmail(to, verifyUrl) {
     const sendSmtpEmail = new brevo.SendSmtpEmail()
     sendSmtpEmail.sender = {
       name: 'Edgy Events',
-      email: process.env.MAIL_FROM || 'no-reply@teanet.syz',
+      email: process.env.MAIL_FROM || 'no-reply@teanet.xyz',
     }
     sendSmtpEmail.to = [{ email: to }]
     sendSmtpEmail.subject = 'Verify your email for Edgy Events'
