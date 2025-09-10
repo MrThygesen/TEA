aa3e9c5035aa5e82e0fec6a05d9dc8a2f5e2b337
MrThygesen <mr.morten.thygesen@gmail.com>
2025-09-10 20:58:20 +0200

    db updates

--- frontend/components/EmailVerified.js
+++ frontend/components/EmailVerified.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import auth from '../lib/auth'
+import auth from '.auth'
 
 export default function EmailVerified() {
   const router = useRouter()
