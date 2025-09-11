9d3b6d6e978e097285c370d57b79d9f1d4800ca9
MrThygesen <mr.morten.thygesen@gmail.com>
2025-09-11 15:22:38 +0200

    db updates

--- frontend/pages/email-verified.js
+++ frontend/pages/email-verified.js
@@ -4,6 +4,3 @@ import EmailVerified from '../components/EmailVerified'
 export default function EmailVerifiedPage() { 
 return <EmailVerified /> }
 
-
-
-git add -f frontend/pages/email-verified.js -f frontend/pages/components/EmailVerified.js -f frontend/pages/api/confirm-email.js
