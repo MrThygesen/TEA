1890085780ab0de8e5cf305b593dcf071bf0efd0
MrThygesen <mr.morten.thygesen@gmail.com>
2025-09-11 15:04:52 +0200

    db updates

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,23 @@
 // pages/_app.js
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+
+import { WagmiConfig } from 'wagmi'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { config } from '../utils/wagmi.config'
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <WagmiConfig config={config}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </QueryClientProvider>
   )
 }
