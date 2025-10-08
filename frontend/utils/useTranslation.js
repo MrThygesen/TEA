bbabe0a21095cee1f3df9651e2bc6e2ddb6dd963
MrThygesen <mr.morten.thygesen@gmail.com>
2025-10-08 10:34:38 +0200

    core updates

--- frontend/utils/useTranslation.js
+++ frontend/utils/useTranslation.js
@@ -2,14 +2,14 @@
 import { useState, useEffect } from 'react'
 import en from '../locales/en.json'
 import da from '../locales/da.json'
-import da from '../locales/de.json'
+import de from '../locales/de.json'
 import fr from '../locales/fr.json'
-import hi from '../locales/ar.json'
-import ar from '../locales/hi.json'
-import ru from '../locales/zh.json'
+import hi from '../locales/hi.json'
+import ar from '../locales/ar.json'
+import ru from '../locales/ru.json'
 import pt from '../locales/pt.json'
 import es from '../locales/es.json'
-import zh from '../locales/ru.json'
+import zh from '../locales/zh.json'
 
 // Static imports = Webpack friendly
 const translations = { en, da, de, fr, hi, ar, ru, pt, es, zh}
