d51fb035d6b78c4653ef2916600bfc20f3f9999d
MrThygesen <mr.morten.thygesen@gmail.com>
2025-10-11 10:48:41 +0200

    core updates

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useAccount } from 'wagmi'
+
+export default function Home() {
+  const { isConnected, address } = useAccount()
+
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>TEA Project Demo</h1>
+      <ConnectButton />
+      {isConnected && <p>Connected wallet: {address}</p>}
+    </main>
+  )
+}
+
+
+
